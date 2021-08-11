08cad14d5845f917e084c62987c35cbda231b604
Alejandro <edarevalo01@gmail.com>
2021-08-10 20:18:26 -0500

    Clase de interpolacion y directivas

--- Frontend/todo-app/src/app/app.component.ts
+++ Frontend/todo-app/src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-app';
+
+  nombre: string = "Atreus";
+  nombreGuardado: string = "";
+  edad: number = 16;
+  casado: boolean = false;
+  listaNombres: Array<string> = ["Alejandro", "Cristian", "Rogger", "Sergio"];
+  //listaNombres: string[] = [];
+
+  guardar() {
+    this.nombreGuardado = this.nombre;
+    this.listaNombres.push(this.nombre);
+  }
+
 }
+
+
+// Creacion de componentes y modelos
+// Enrutamiento
