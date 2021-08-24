a07e962e6520eaee5043097e5cbcfb801d32239f
Alejandro <edarevalo01@gmail.com>
2021-08-23 19:24:42 -0500

    proyecto base

--- Frontend/todo-app/src/app/app.component.ts
+++ Frontend/todo-app/src/app/app.component.ts
@@ -7,26 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-app';
-
-  nombre: string = "Atreus";
-  nombreGuardado: string = "";
-  edad: number = 16;
-  casado: boolean = false;
-  listaNombres: Array<string> = ["Alejandro", "Cristian", "Rogger", "Sergio"];
-  persona = {
-    name: "alejandro",
-    age: 24,
-    married: false
-  }
-  //listaNombres: string[] = [];
-
-  guardar() {
-    this.nombreGuardado = this.nombre;
-    this.listaNombres.push(this.nombre);
-  }
-
 }
-
-
-// Creacion de componentes y modelos
-// Enrutamiento
