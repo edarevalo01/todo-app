450cd7c4d1728ca7ccd88a796c3506c69cd30b2e
Alejandro <edarevalo01@gmail.com>
2021-08-23 20:43:31 -0500

    Componentes base

--- Frontend/todo-app/src/app/app.module.ts
+++ Frontend/todo-app/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './components/header/header.component';
+import { ActionBarComponent } from './components/action-bar/action-bar.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ListComponent } from './pages/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ActionBarComponent,
+    LoginComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
